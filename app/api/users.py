b1234ac81507ccaa09568dff5f75c91e6ac7452a
laporkka <alexsolodkiy2008@gmail.com>
2026-08-14 04:02:59 +0300

    feat: complete user auth endpoint with oauth2 form validation

--- app/api/users.py
+++ app/api/users.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
 from app.schemas.users import UserCreate, UserResponse
+from app.schemas.users import Token
 from app.services.users import get_user_by_email, create_user
+from app.core.security import verify_password, create_access_token
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -17,13 +19,33 @@ async def register_user(
     existing_user = await get_user_by_email(db, user_in.email)
 
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
 
     new_user = await create_user(db, user_in)
 
     return new_user
 
 
+@router.post("/login", response_model=Token)
+async def login_user(
+    user_in: UserCreate,
+    db: AsyncSession = Depends(get_db)
+):
+    user = await get_user_by_email(db, user_in.email)
 
-        
+    if not user or not verify_password(user_in.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not exist",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    access_token = create_access_token({"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+    
     
--- app/tasks/worker.py
+++ app/tasks/worker.py
@@ -23,7 +23,7 @@ celery_app.conf.update(
 )
 
 
-celery_app.autodiscover_tasks(["tasks"])
+celery_app.autodiscover_tasks(["app.tasks"])
 
 
 @celery_app.task
